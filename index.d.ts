b21939d8ea85df89e0399e622f7c7f69c803d059
flakey5 <73616808+flakey5@users.noreply.github.com>
2024-10-08 11:01:52 -0700

    fix type tests
    Signed-off-by: flakey5 <73616808+flakey5@users.noreply.github.com>

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,6 @@
+import { Readable, Writable } from "node:stream";
 import { RedisOptions } from "iovalkey";
-import RedisCacheStore from "./lib/redis-cache-store";
+import { Dispatcher } from "undici";
 
 export interface RedisCacheStoreOpts {
   clientOpts?: RedisOptions
@@ -11,6 +12,51 @@ export interface RedisCacheStoreOpts {
    * @default true
    */
   tracking?: boolean
+}
+
+declare class RedisCacheStore {
+  constructor(opts?: RedisCacheStoreOpts);
+
+  get isFull(): boolean
+
+  close(): Promise<void>
+
+  createReadStream(req: Dispatcher.RequestOptions): Promise<RedisStoreReadable | undefined>
+
+  createWriteStream(req: Dispatcher.RequestOptions, opts: CacheStoreValue): RedisStoreWritable | undefined
+}
+
+interface RedisStoreReadable extends Readable {
+  get value(): CacheStoreValue
+}
+
+interface RedisStoreWritable extends Writable {
+  set rawTrailers(trailers: string[] | undefined)
+}
+
+interface CacheStoreValue {
+  statusCode: number;
+  statusMessage: string;
+  rawHeaders: (Buffer | Buffer[])[];
+  rawTrailers?: string[];
+  /**
+   * Headers defined by the Vary header and their respective values for
+   *  later comparison
+   */
+  vary?: Record<string, string>;
+  /**
+   * Time in millis that this value was cached
+   */
+  cachedAt: number;
+  /**
+   * Time in millis that this value is considered stale
+   */
+  staleAt: number;
+  /**
+   * Time in millis that this value is to be deleted from the cache. This is
+   *  either the same as staleAt or the `max-stale` caching directive.
+   */
+  deleteAt: number;
 }
 
 export {
