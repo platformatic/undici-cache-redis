339d4ccff729ff3bf26200c2d4250a759f67e6d5
Ivan Tymoshenko <ivan@tymoshenko.me>
2025-06-06 16:34:25 +0200

    fix: CacheManager cache invalidation (#31)

--- test/cache-manager.test.js
+++ test/cache-manager.test.js
@@ -2,7 +2,12 @@
 
 const assert = require('node:assert/strict')
 const { test } = require('node:test')
-const { RedisCacheManager } = require('../index.js')
+const { once } = require('node:events')
+const { randomUUID } = require('node:crypto')
+const { setTimeout: sleep } = require('node:timers/promises')
+const { createServer } = require('node:http')
+const { Client, interceptors } = require('undici')
+const { RedisCacheManager, RedisCacheStore } = require('../index.js')
 const { cleanValkey } = require('./helper.js')
 
 test('cache manager works when notify-keyspace-events is configured on the server', async (t) => {
@@ -31,4 +36,93 @@ test('cache manager fails when notify-keyspace-events is NOT configured on the s
   t.after(async () => {
     await manager.close()
   })
+})
+
+test('invalidates response from cache manager', async (t) => {
+  await cleanValkey()
+
+  const keyPrefix = `${randomUUID()}:`
+
+  // In the ICC we use RedisCacheManager without any keyPrefix to have a single
+  // instance for all applications
+  const manager = new RedisCacheManager()
+  await manager.subscribe()
+  t.after(() => manager.close())
+
+  let requestsToOrigin = 0
+
+  const server = createServer((_, res) => {
+    requestsToOrigin++
+    res.setHeader('cache-control', 'public, s-maxage=20000')
+    res.end('asd')
+  }).listen(0)
+
+  await once(server, 'listening')
+
+  const store = new RedisCacheStore({
+    clientOpts: { keyPrefix }
+  })
+
+  const origin = `http://localhost:${server.address().port}`
+  const client = new Client(origin).compose(interceptors.cache({ store }))
+
+  t.after(async () => {
+    server.close()
+    await store.close()
+    await client.close()
+  })
+
+  assert.strictEqual(requestsToOrigin, 0)
+
+  {
+    // Send initial request. This should reach the origin
+    const { statusCode, body } = await client.request({
+      origin, method: 'GET', path: '/'
+    })
+    assert.strictEqual(statusCode, 200)
+    assert.strictEqual(requestsToOrigin, 1)
+
+    const text = await body.text()
+    assert.strictEqual(text, 'asd')
+  }
+
+  // Wait for redis to save the response
+  await sleep(1000)
+
+  {
+    // Send second request that should be handled by cache
+    const { statusCode, body, headers } = await client.request({
+      origin, method: 'GET', path: '/'
+    })
+    assert.strictEqual(statusCode, 200)
+    assert.strictEqual(requestsToOrigin, 1)
+
+    const text = await body.text()
+    assert.strictEqual(text, 'asd')
+    assert.strictEqual(headers.age, '1')
+  }
+
+  const foundEntries = []
+  await manager.streamEntries(entry => {
+    foundEntries.push(entry)
+  }, keyPrefix)
+  assert.strictEqual(foundEntries.length, 1)
+
+  const foundEntity = foundEntries[0]
+  assert.ok(foundEntity.id)
+
+  await manager.deleteIds([foundEntity.id], keyPrefix)
+  await sleep(1000)
+
+  {
+    // Send third request that should reach the origin again
+    const { statusCode, body } = await client.request({
+      origin, method: 'GET', path: '/'
+    })
+    assert.strictEqual(statusCode, 200)
+    assert.strictEqual(requestsToOrigin, 2)
+
+    const text = await body.text()
+    assert.strictEqual(text, 'asd')
+  }
 })
